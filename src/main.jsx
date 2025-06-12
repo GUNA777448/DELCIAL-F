cf714d31a8ab903838f7e9d187262d5e18e53086
GurunadaRao <gurunadharao5718@gmail.com>
2025-06-12 20:47:08 +0530

    git check 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/navbar.jsx";
-import Chatbot from "./components/chatbot.jsx";
+
+import Chatbot from "./components/Chatbot.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
