a75fedd83748c74f459685d4f860d973844740c7
GurunadaRao <gurunadharao5718@gmail.com>
2025-07-09 22:40:20 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/", // this is okay if hosted at root
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // important for dev reloading
+  }
 });
